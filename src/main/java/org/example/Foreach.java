a5ef0f2620d868a0069b5d82829c167c3cafea27
kateshiAIV <148715019+kateshiAIV@users.noreply.github.com>
2025-03-05 18:25:08 +0100

    new commit

--- src/main/java/org/example/Foreach.java
+++ src/main/java/org/example/Foreach.java
@@ -4,16 +4,13 @@ import java.util.Scanner;
 
 public class Foreach {
     public static void main(String args[]){
-        int table[][] = new int[3][5];
-        for (int i =0;i<3;i++){
-            for (int j= 0; j<5;j++) {
-                table[i][j] = (j + 1) * (i + 1);
-            }
+
+        for(int i = 0; i <10;i++){
+            System.out.print(";");
+            System.out.print(i + " ");
+            if(i%2==0)continue;
+            System.out.println(":");
         }
-        for(var x: table){
-            for(var y:x){
-                System.out.print(y + " ");
-            }
-        }
+
     }
 }
